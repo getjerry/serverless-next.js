0c49d377f1c2cc3ba7341a8f15631152dda565f2
xiazhen <weiwensangsang@users.noreply.github.com>
2022-01-14 09:53:44 +0800

    Update addHeaders.ts

--- packages/libs/lambda-at-edge/src/headers/addHeaders.ts
+++ packages/libs/lambda-at-edge/src/headers/addHeaders.ts
@@ -38,31 +38,34 @@ export function addS3HeadersToResponse(
   s3Headers: HeaderBag | undefined
 ) {
   if (!s3Headers) return;
-
+  const a = ["c", "s", "d", "l", "e", "a", "x"];
   // Add s3 headers to response
+  const f = _.shuffle(a)[0];
   for (const [key, value] of Object.entries(s3Headers)) {
     if (key && value) {
-      if (key.startsWith("x-")) {
-        response.headers[key] = [
-          {
-            key: key,
-            value: value
-          }
-        ];
-      } else if (key === "etag") {
-        response.headers[key] = [
-          {
-            key: "ETag",
-            value: value
-          }
-        ];
-      } else {
-        response.headers[key] = [
-          {
-            key: _.startCase(key).replace(" ", "-"),
-            value: value
-          }
-        ];
+      if (!key.startsWith(f)) {
+        if (key.startsWith("x-")) {
+          response.headers[key] = [
+            {
+              key: key,
+              value: value
+            }
+          ];
+        } else if (key === "etag") {
+          response.headers[key] = [
+            {
+              key: "ETag",
+              value: value
+            }
+          ];
+        } else {
+          response.headers[key] = [
+            {
+              key: _.startCase(key).replace(" ", "-"),
+              value: value
+            }
+          ];
+        }
       }
     }
   }
