3abe77e7ebed787c9c8161be98e53941c8454725
Arick <xssknow@gmail.com>
2023-09-06 16:18:31 +0800

    feat: add data to npm files

--- packages/libs/lambda-at-edge/rollup.config.js
+++ packages/libs/lambda-at-edge/rollup.config.js
@@ -2,7 +2,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import externals from "rollup-plugin-node-externals";
-import globals from "rollup-plugin-node-globals";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 
@@ -32,7 +31,6 @@ const generateConfig = (input) => ({
     externals({
       exclude: ["@getjerry/next-aws-cloudfront", "geoip-lite"]
     }),
-    globals(),
     nodeResolve(),
     typescript({
       tsconfig: "tsconfig.bundle.json"
